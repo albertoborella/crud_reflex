d9c00087dc5b3d588ea2de7fe7400ea945f4b889
Alberto Borella <albertofborella@gmail.com>
2026-02-18 19:09:21 -0300

    modificacion de rxconfig y variable de entorno

--- rxconfig.py
+++ rxconfig.py
@@ -1,11 +1,24 @@
 import reflex as rx
+import os
+from dotenv import load_dotenv
+
+# Esto asegura que busque el .env en la misma carpeta donde está este script
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
+
+db_url = os.getenv("DATABASE_URL")
+
+# Verificación rápida (solo para debug, después podés borrar el print)
+if not db_url:
+    print("⚠️ Error: No se pudo cargar DATABASE_URL del archivo .env")
 
 config = rx.Config(
     app_name="app",
-    db_url="sqlite:///alquileres.db",
+    db_url=db_url,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ]
 )
+
 
